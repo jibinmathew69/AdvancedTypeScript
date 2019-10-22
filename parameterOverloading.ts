74d1a58746482ae4b86cde9dcf72111d1ffb96cb
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 15:06:01 +0530

    Add in operation for type gaurd

--- parameterOverloading.ts
+++ parameterOverloading.ts
@@ -14,14 +14,23 @@ interface User{
     email: string
 }
 
+// function redirect(usr : Admin | User){
+//     if (isAdmin(usr)){
+//         routeToAdminPage(usr.role)
+//     }else{
+//         routeToHomePage(usr.email)
+//     }
+// }
+
+// function isAdmin(usr: Admin | User): usr is Admin{
+//     return (<Admin>usr).role !== undefined
+// }
+
 function redirect(usr : Admin | User){
-    if (isAdmin(usr)){
+    if ("role" in usr){
         routeToAdminPage(usr.role)
     }else{
         routeToHomePage(usr.email)
     }
 }
 
-function isAdmin(usr: Admin | User): usr is Admin{
-    return (<Admin>usr).role !== undefined
-}
