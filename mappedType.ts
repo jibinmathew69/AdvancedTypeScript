0b445113a9e65bc76f71cd8fc0dee52c3e50f1cd
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 16:00:50 +0530

    Mapped type for remiving optional

--- mappedType.ts
+++ mappedType.ts
@@ -1,14 +1,14 @@
 interface IPet{
     name: String
     age: number
+    fav?: number
 }
 
 type ReadOnlyPet = {
-    readonly [K in keyof IPet]: IPet[K]
+    readonly [K in keyof IPet]-?: IPet[K]
 }
 
 let pet:IPet = {name: "Tom", age: 3}
 let readPet:ReadOnlyPet = {name: "Tom", age: 3}
 
 pet.age = 2
-readPet.age = 2 // throws error
