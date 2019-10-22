adcececec2840a77fc71aa4841859012a9802773
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 14:46:59 +0530

    Add strict null check

---
+++ nullType.ts
@@ -0,0 +1,13 @@
+class Library{
+    titles: string[]
+    
+    constructor() {
+        
+    }   
+}
+
+let library = new Library()
+
+let shortTitles = library.titles.filter(
+    title => title.length < 5
+)
