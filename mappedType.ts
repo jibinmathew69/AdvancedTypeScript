629661985dbffba2e00029e216147eb5b2be94ab
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 15:58:12 +0530

    Create and modify readonly instance

--- mappedType.ts
+++ mappedType.ts
@@ -6,3 +6,9 @@ interface IPet{
 type ReadOnlyPet = {
     readonly [K in keyof IPet]: IPet[K]
 }
+
+let pet:IPet = {name: "Tom", age: 3}
+let readPet:ReadOnlyPet = {name: "Tom", age: 3}
+
+pet.age = 2
+readPet.age = 2 // throws error
